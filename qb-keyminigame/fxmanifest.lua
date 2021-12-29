3d7e4a03af0217819e24ad05fe3432a1510e1502
nixity <87250559+nixity@users.noreply.github.com>
2021-12-28 19:02:50 -0800

    Add files via upload

---
+++ qb-keyminigame/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'bodacious'
+game 'gta5'
+
+description 'QB-KeyMiniGame'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+client_scripts {
+    '@qb-core/import.lua',
+    'client/main.lua',
+}
+
+files {
+    'html/index.html',
+    'html/app.js',
+    'html/style.css',
+}
---
+++ qb-logs/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'bodacious'
+game 'gta5'
+
+description 'QB-Logs'
+version '1.0.0'
+
+shared_script '@qb-core/import.lua'
+
+server_scripts {
+    'server/server.lua',
+    'config.lua'
+}
