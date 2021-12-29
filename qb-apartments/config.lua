e6bd37a6b245c0686fd1956df5d79b242dcefef2
nixity <87250559+nixity@users.noreply.github.com>
2021-12-28 18:51:51 -0800

    Add files via upload

---
+++ qb-apartments/config.lua
@@ -0,0 +1,14 @@
+Apartments = {}
+
+Apartments.SpawnOffset = 30
+
+Apartments.Locations = {
+    ["apartment1"] = {
+        name = "apartment1",
+        label = "3 Alta Street",
+        coords = {
+            enter = vector4(-271.12, -957.91, 31.22, 122.95),
+            doorbell = vector4(-270.19, -960.13, 31.22, 120.54)
+        }
+    },
+}
